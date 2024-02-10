e107be38827de0d9cada79ef0a3b1e2967b9ac92
boylesz <boyleszavi@myvuw.ac.nz>
2024-02-11 01:22:17 +1300

    About section progress

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // Responsive Toggle Navigation
 let menuIcon = document.querySelector('.menuIcon');
 let nav = document.querySelector('.overlay-menu');
+let navMenu = document.querySelector('.overlay-menu #menu');
 
 menuIcon.addEventListener('click', () => {
     if (nav.style.transform != 'translateX(0%)') {
@@ -14,13 +15,20 @@ menuIcon.addEventListener('click', () => {
     }
 });
 
+navMenu.addEventListener('click', () => {
+    nav.style.transform = 'translateX(-100%)';
+    nav.style.transition = 'transform 0.2s ease-out';
+});
+
 // Toggle Menu Icon ========================================
 let toggleIcon = document.querySelector('.menuIcon');
 
-toggleIcon.addEventListener('click', () => {
+toggleIcon.addEventListener('click', toggleMenuIcon);
+
+function toggleMenuIcon() {
     if (toggleIcon.className != 'menuIcon toggle') {
         toggleIcon.className += ' toggle';
     } else {
         toggleIcon.className = 'menuIcon';
     }
-});
+}
