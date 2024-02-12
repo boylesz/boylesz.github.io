42e4fe7ce698440923a5bcfcf0667e171e5c3e86
boylesz <boyleszavi@myvuw.ac.nz>
2024-02-12 22:28:50 +1300

    Update games_script.js

--- pages/games/games_script.js
+++ pages/games/games_script.js
@@ -1,3 +1,5 @@
+var request = new XMLHttpRequest();
+
 const apiKey = 'iSgeQrUMdbdatfho34urIBkxTJvQF4wgRtaZnKGb';
 const apiURL = 'https://itch.io/api/1/KEY/my-games';
 
@@ -10,16 +12,24 @@ const requestOptions = {
     }
 };
 
-fetch(apiURL, requestOptions)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        outputElement.textContent = JSON.stringify(data, null, 2);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+request.open("GET", apiURL);
+request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+        outputElement.textContent = JSON.stringify(request.responseText, null, 2);
+    }
+}
+request.send();
+
+// fetch(apiURL, requestOptions)
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         outputElement.textContent = JSON.stringify(data, null, 2);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
