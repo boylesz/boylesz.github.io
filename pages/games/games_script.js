97d63b6489a7b7a39391e386819bfa7e4432cde4
boylesz <boyleszavi@myvuw.ac.nz>
2024-02-12 23:40:11 +1300

    Update games_script.js

--- pages/games/games_script.js
+++ pages/games/games_script.js
@@ -1,35 +1,19 @@
-var request = new XMLHttpRequest();
-
 const apiKey = 'iSgeQrUMdbdatfho34urIBkxTJvQF4wgRtaZnKGb';
 const apiURL = 'https://itch.io/api/1/KEY/my-games';
 
 const outputElement = document.querySelector('.exampleElement');
 
-const requestOptions = {
-    method: 'GET',
+const fetchPromise = fetch(apiURL, {
+    method: "GET",
+    mode: "cors",
     headers: {
-        'Authorization': `Bearer ${apiKey}`
+        "X-PINGOTHER": "pingpong",
+        "Access-Control-Allow-Method": "GET",
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
     }
-};
+});
 
-request.open("GET", apiURL);
-request.onreadystatechange = function() {
-    if (request.readyState == 4) {
-        outputElement.textContent = JSON.stringify(request.responseText, null, 2);
-    }
-}
-request.send();
-
-// fetch(apiURL, requestOptions)
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         outputElement.textContent = JSON.stringify(data, null, 2);
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//     });
+fetchPromise.then(response => {
+    console.log(response.status);
+})
